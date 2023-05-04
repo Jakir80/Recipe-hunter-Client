57bd2838a977019a00410e2a0e7c3a92312e6ceb
Jakir80 <jh7593617@gmail.com>
2023-05-04 06:52:41 -1100

    edit-something-and-hide-firebase

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,18 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-console.log( "SECRET" , import.meta.env.VITE_apiKey)
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyD3IfLpT6o4d4LSTuc_OznnvCgTktWl8o0",
-//   authDomain: "chef-time-assignment.firebaseapp.com",
-//   projectId: "chef-time-assignment",
-//   storageBucket: "chef-time-assignment.appspot.com",
-//   messagingSenderId: "577776980680",
-//   appId: "1:577776980680:web:5ec4c5512d77984af0cbfd"
-// };
 
+import { initializeApp } from "firebase/app";
+
+console.log( "SECRET" , import.meta.env.VITE_apiKey)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain:import.meta.env.VITE_authDomain,
